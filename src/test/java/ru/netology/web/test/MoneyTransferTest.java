a72c2fe5413a7da32b552c0e74793f5d8c4e5026
Atlas Vlad <vlad269123@gmail.com>
2025-05-06 01:07:39 +0300

    Add server JAR

--- src/test/java/ru/netology/web/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/web/test/MoneyTransferTest.java
@@ -1,57 +1,51 @@
 package ru.netology.web.test;
 
-import org.junit.jupiter.api.BeforeEach;
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
 import ru.netology.web.data.DataHelper;
-import ru.netology.web.page.DashboardPage;
 import ru.netology.web.page.LoginPage;
 
-import static com.codeborne.selenide.Selenide.open;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static ru.netology.web.data.DataHelper.*;
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static ru.netology.web.data.DataHelper.getAuthInfo;
 
 class MoneyTransferTest {
-    DashboardPage dashboardPage;
-    DataHelper.CardInfo firstCardInfo;
-    DataHelper.CardInfo secondCardInfo;
+    @Test
+    void shouldTransferMoneyBetweenOwnCardsV1() {
+        var info = getAuthInfo();
+        var verificationCode = DataHelper.getVerificationCodeFor(info);
+        var cardFirstInfo = DataHelper.getFerstCardInfo();
+        var cardSecondInfo = DataHelper.getSecondCardInfo();
+        var amount100 = DataHelper.genTransferNumber();
 
-    @BeforeEach
-    void setUp() {
-        var loginPage = open("http://localhost:9999", LoginPage.class);
-        var authInfo = getAuthInfo();
-        var verificationPage = loginPage.validLogin(authInfo);
-        var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
-        dashboardPage = verificationPage.validVerify(verificationCode);
-        firstCardInfo = getFirstCardInfo();
-        secondCardInfo = getSecondCardInfo();
+        var loginPage = Selenide.open("http://localhost:9999", LoginPage.class);
+        var verificationPage = loginPage.validLogin(info);
+        var dashboardPage = verificationPage.validVerification(verificationCode);
+        var transferCard = dashboardPage.selectCard(cardFirstInfo);
+        transferCard.enterTransferAmount(amount100, cardSecondInfo);
+        dashboardPage.updateCardBalance(cardFirstInfo, amount100);
     }
 
     @Test
-    void shouldTransferFromFirstCardToSecond() {
-        var firstCardBalance = dashboardPage.getCardBalance(firstCardInfo);
-        var secondCardBalance = dashboardPage.getCardBalance(secondCardInfo);
-        var amount = generateValidAmount(firstCardBalance);
-        var expectedBalanceFirstCard = firstCardBalance - amount;
-        var expectedBalanceSecondCard = secondCardBalance + amount;
-        var transferPage = dashboardPage.selectCardToTransfer(secondCardInfo);
-        dashboardPage = transferPage.makeValidTransfer(String.valueOf(amount), firstCardInfo);
-        var actualBalanceFirstCard = dashboardPage.getCardBalance(firstCardInfo);
-        var actualBalanceSecondCard = dashboardPage.getCardBalance(secondCardInfo);
-        assertEquals(expectedBalanceFirstCard, actualBalanceFirstCard);
-        assertEquals(expectedBalanceSecondCard, actualBalanceSecondCard);
-    }
+    void shouldTransferMoneyBetweenOwnCards() {
+        DataHelper.AuthInfo info = getAuthInfo();
+        DataHelper.VerificationCode verificationCode = DataHelper.getVerificationCodeFor(info);
+        DataHelper.CardInfo cardFirstInfo = DataHelper.getFerstCardInfo();
+        DataHelper.getSecondCardInfo();
+        DataHelper.genTransferNumber();
 
-    @Test
-    void shouldGetErrorMessageIfAmountMoreBalance() {
-        var firstCardBalance = dashboardPage.getCardBalance(firstCardInfo);
-        var secondCardBalance = dashboardPage.getCardBalance(secondCardInfo);
-        var amount = generateInvalidAmount(secondCardBalance);
-        var transferPage = dashboardPage.selectCardToTransfer(firstCardInfo);
-        transferPage.makeTransfer(String.valueOf(amount), secondCardInfo);
-        transferPage.findErrorMessage("На карте недостаточно средств");
-        var actualBalanceFirstCard = dashboardPage.getCardBalance(firstCardInfo);
-        var actualBalanceSecondCard = dashboardPage.getCardBalance(secondCardInfo);
-        assertEquals(firstCardBalance, actualBalanceFirstCard);
-        assertEquals(secondCardBalance, actualBalanceSecondCard);
+        var loginPage = Selenide.open("http://localhost:9999", LoginPage.class);
+        var verificationPage = loginPage.validLogin(info);
+        var dashboardPage = verificationPage.validVerification(verificationCode);
+        var transferCard = dashboardPage.selectCard(cardFirstInfo);
+        $("[data-test-id='dashboard']");
+        $("[class=App_appContainer__3jRx1]");
+        $("[data-test-id=amount] .input__control").shouldBe(visible).setValue("100");
+        $("[data-test-id=from] .input__control").shouldBe(visible).setValue("5559 0000 0000 0002");
+        $(".button").click();
+        $("[data-test-id=92df3f1c-a033-48e6-8390-206f6b1f56c0] .list__item").shouldHave(Condition.text("10100")).shouldBe(visible);
     }
 }
